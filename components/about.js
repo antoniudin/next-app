24adcbd77f3d8bbae5f8f4a8c720530fe063b651
Anton Iudin <yudin431@gmail.com>
2022-06-22 18:32:22 -0400

    link updated

--- components/about.js
+++ components/about.js
@@ -8,7 +8,7 @@ export default function About() {
             <p>Hi, my name is</p>
             {/* </div> */}
             <h2>Anton Iudin</h2>
-            
+
         <p>I am a software engineer with previous experience in IT, which I use working on various projects.</p>
         
         <div className={styles.spacer}></div>
@@ -16,7 +16,7 @@ export default function About() {
             </p>
             
             <div className={styles.socials}>
-                <a  href="https://drive.google.com/file/d/1naw-eIYAzQgwKKh8bJasNjkJRbs7h-S3/view?usp=sharing" className={styles.techstack_item}>Resume</a>
+                <a  href="bit.ly/3HLObrN" className={styles.techstack_item}>Resume</a>
                 <a href="https://bit.ly/3Nc8aRl" className={styles.techstack_item}>Slides</a>
                 <a href="https://github.com/antoniudin" className={styles.github}></a>
                 <a href="https://www.linkedin.com/in/ai431/" className={styles.youtube}></a>
